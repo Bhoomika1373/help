62c1c9186bdf6d4195e3aec23147c6a2c98a5574
Bhomika K S <159525891+Bhoomika1373@users.noreply.github.com>
2024-10-18 13:55:34 +0530

    Update sidebar.tsx

--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -1,15 +1,38 @@
-import '../styles/globals.css';
-import Sidebar from '../components/Sidebar';
+import Link from "next/link";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-grow p-6">
-        <Component {...pageProps} />
-      </div>
-    </div>
-  );
+export default function SideBar() {
+
+    return (
+<div className="flex">
+<div className="h-full w-1/8 text-black">
+<div className="p-4">
+  <h2 className="text-lg font-bold ">Category</h2>
+</div>
+<nav>
+  <ul className="space-y-2">
+    <li>
+      <Link href="/" className="block p-2 hover:bg-gray-700 rounded">
+        Home
+      </Link>
+    </li>
+    <li>
+      <Link href="/about" className="block p-2 hover:bg-gray-700 rounded">
+        About
+      </Link>
+    </li>
+    <li>
+      <Link href="/services" className="block p-2 hover:bg-gray-700 rounded">
+        Services
+      </Link>
+    </li>
+    <li>
+      <Link href="/contact" className="block p-2 hover:bg-gray-700 rounded">
+        Contact
+      </Link>
+    </li>
+  </ul>
+</nav>
+</div>
+</div>
+);
 }
-
-export default MyApp;
